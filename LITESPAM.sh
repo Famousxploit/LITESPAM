24a97c095e34b6bc700594267062df5d56b9929c
Famousxploit <37292122+Famousxploit@users.noreply.github.com>
2018-03-12 16:53:10 +0700

    Create LITESPAM.sh

---
+++ LITESPAM.sh
@@ -0,0 +1,58 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "LiteSpam"
+sleep 1
+echo "\033[31;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[33;1m Author: Walker#04"
+sleep 1
+echo "\033[32;1m Contact: +16285006979"
+sleep 1
+echo "\033[34;1m Famous Xploit Team"
+sleep 1
+echo "\033[36;1m Spam Yang Tersedia :"
+echo "1. Telkomsel"
+echo "0. Keluar"
+echo "\033[30;1m81. Informasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if
+[ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+php 2.php
+fi
+
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "LiteSpam"
+echo "\033[31;1mNama tools: Litespam"
+sleep 1
+echo "\033[33;1mAuthor: Walker#04"
+sleep 1
+echo "\033[32;1mVersi: v1"
+sleep 1
+echo "\033[36;1mTeam: Famous Xploit Team"
+sleep 1
+echo "\033[31;1m Spesial Thanks To: "
+
+sleep 1
+echo "All Member Famous Xploit"
+echo "\033[30;1m tunggu 5 detik"
+sleep 5
+sh LITESPAM.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m See You Later! :)"
+sleep 1
+fi
